ab45d88410a6016042e5c6e4ae5112f474c68528
Ilza <ilzammed2@gmail.com>
2020-06-23 16:23:53 -0300

    archives unnecessary deleted

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import './App.css';
+import data from './data.json';
+import Home from './Pages/Home/Home'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       
+        <Home data={data}/>
     </div>
   );
 }
