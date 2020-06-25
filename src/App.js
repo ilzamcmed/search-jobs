c2d1021a48adb9e0169a918b3348a0b57f7b12e9
Ilza <ilzammed2@gmail.com>
2020-06-25 14:02:47 -0300

    updated to show only login page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import Home from './Pages/Home/Home'
-// import Login from './Pages/Login/Login';
-import data from './data.json';
+// import Home from './Pages/Home/Home'
+import Login from './Pages/Login/Login';
+// import data from './data.json';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <Home data={data}/>
-        {/* <Login /> */}
+        {/* <Home data={data}/> */}
+        <Login />
     </div>
   );
 }
